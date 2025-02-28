c765adbc034453137ef1c43c586b8e6d6895fd97
GXC2356 <gxcdev2356@gmail.com>
2025-02-28 12:22:53 +0200

    vendorsetup: Add vendorsetup.sh file

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# Common
+git clone https://github.com/LineageOS/android_device_samsung_gta4l-common.git device/samsung/gta4l-common
+
+# Vendor
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_gta4l.git vendor/samsung/gta4l
+
+# Vendor common
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_gta4l-common.git  vendor/samsung/gta4l-common
+
+# Kernel
+git clone https://github.com/LineageOS/android_kernel_samsung_sm6115.git kernel/samsung/sm6115
+
+# Hardware
+git clone https://github.com/LineageOS/android_hardware_samsung.git hardware/samsung
